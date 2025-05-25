0b68fce0891687fab4130eff43eb6e9280bfc7f0
lmacionis <lmacionis@yahoo.com>
2025-05-25 22:34:13 +0300

    Exported iou function and save model from tests to according files

---
+++ not_from_zero_but_still_to_hero/intersection_over_union.py
@@ -0,0 +1,13 @@
+import torch
+
+
+def iou_score(pred, target, threshold=0.5):
+        pred = torch.tensor(pred, device=device)
+        target = torch.tensor(target, device=device)
+        pred = (pred > threshold).float()
+        intersection = (pred * target).sum()
+        union = pred.sum() + target.sum() - intersection
+        iou = (intersection + 1e-5) / (union + 1e-5)
+        # Check intersection and union
+        print(f"Intersection: {intersection}, Union: {union}")
+        return iou
---
+++ not_from_zero_but_still_to_hero/save_model.py
@@ -0,0 +1,11 @@
+def save_model(unet_model, optimizer, epoch, train_losses, train_accuracies, val_losses, val_accuracies, path="/model.pth"):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': unet_model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'train_losses': train_losses,
+        'train_accuracies': train_accuracies,
+        'val_losses': val_losses,
+        'val_accuracies': val_accuracies
+    }, path)
+    print(f"Model saved to {path}")
